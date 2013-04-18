0eac44c4afa8310cd51f9f36defd2273a75d0b7e
Dmitry Nikifortsev <dimyan@gmail.com>
2013-04-18 14:08:27 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flying::Application.config.secret_key_base = '32b207c6d1d1797fd20f0e704cfcc2ede3ee111c12153f7b886a1756925551be39a8d007603ff6eab6820166d64da23b6c70e7ab02b5475716046f727e7efada'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flying::Application.config.session_store :encrypted_cookie_store, key: '_flying_session'
